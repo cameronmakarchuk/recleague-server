3e47277bc2a25d6d8c4b143d1f862b101b25ee60
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-10-26 17:01:08 -0400

    Create migration file for table creation

---
+++ migrations/20221026202708_create_recleague_tables.js
@@ -0,0 +1,43 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('users', (table) => {
+            table.incrementts('id').primary();
+            table.string('first_name').notNullable();
+            table.string('last_name').notNullable();
+            table.string('email').notNullable();
+            table.string('username').notNullable();
+            table.string('avatar_url').notNullable();
+            table.timestamp('updated_at').defaultTo(knex.fn.now());
+        })
+        .createTable('leagues', (table) => {
+            table.increments('id').primary();
+            table.integer('league_owner').unsigned().notNullable();
+            table.string('name').notNullable();
+            table.string('sport').notNullable();
+            table.string('gender').notNullable();
+            table.string('address').notNullable();
+            table.integer('price').notNullable();
+            table.string('start_date').notNullable();
+            table.string('end_date').notNullable();
+            table.text('description').notNullable();
+            table.foreign('league_owner').references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
+        })
+        .createTable('league_details', (table) => {
+            table.integer('users_id').unsigned().notNullable();
+            table.integer('leagues_id').unsigned().notNullable();
+            table.foreign('users_id').references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
+            table.foreign('leages_id').references('id').inTable('leagues').onUpdate('CASCADE').onDelete('CASCADE');
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('users').dropTable('leagues').dropTable('league_details');
+};
