9a7f9371362dec8db1fb958fa1339e77b4214ec0
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-05 12:59:56 -0400

    Initialize set up for GET leagues joined endpoint in league_details.js

--- routes/league_details.js
+++ routes/league_details.js
@@ -16,6 +16,27 @@ router.post('/', (req, res) => {
         .catch(err => res.status(400).send(`Error creating your record: ${err}`));
 })
 
+router.get('/leagues/:userId', (req, res) => {
+    const { userId } = req.params;
+    knex.select('users.id', 'users.first_name', 'leagues.id', 'leagues.name')
+        .from('users')
+        .join('league_details', { 'league_details.users_id': 'users.id' })
+        .join('leagues', { 'leagues.id': 'league_details.leagues_id' })
+        .then(resp => res.status(200).json(resp))
+        .catch(err => res.status(400).json(err))
+
+})
+
+/* 
+SELECT users.id, users.first_name, leagues.id, leagues.name
+FROM leagues
+JOIN league_details
+ON (league_details.leagues_id = leagues.id)
+JOIN users
+ON (users.id = league_details.users_id);
+*/
+
+
 
 
 
