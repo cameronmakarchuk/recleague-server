b978d8e3e3335050a4a780941b321f845b4af38d
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-09 18:03:09 -0500

    Move SQL user and password from knexfile.js into .env

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,8 @@ module.exports = {
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
-    user: 'root',
-    password: 'rootroot',
+    user: process.env.SQL_USER ?? 'root',
+    password: process.env.SQL_PASSWORD ?? 'rootroot',
     database: 'recleague',
     charset: 'utf8'
   },
